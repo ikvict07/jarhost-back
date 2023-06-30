f36b869778ad655e8603c8982c1692f6d511247b
Yaroslav <101941791+yaroslavtok06@users.noreply.github.com>
2023-06-30 19:22:22 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_create, name='post_create'),
-    path('post/<int:pk>/edit/', views.post_update, name='post_update'),
-    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('', views.post_list),
+    path('post/<int:pk>/', views.post_detail),
+    path('post/new/', views.post_create),
+    path('post/<int:pk>/edit/', views.post_update),
+    path('post/<int:pk>/delete/', views.post_delete),
 ]
