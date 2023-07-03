6f1c62f37706792df27b7ae68a0c565c6425013c
Netik Viacheslav <slava5582@gmail.com>
2023-07-03 21:56:05 +0200

    Change Post to News, but must be Squad

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
-from .models import Post
+from .models import News
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = News.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(News, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
 def post_create(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(News, pk=pk)
     return render(request, 'blog/post_create.html', {'post': post})
 
+
 def post_update(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(News, pk=pk)
     return render(request, 'blog/post_update.html', {'post': post})
 
+
 def post_delete(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(News, pk=pk)
     return render(request, 'blog/post_delete.html', {'post': post})
