6cc2c7d437186eb8ab9285e0f6005083f567bd77
Anton <103181179+ikvict07@users.noreply.github.com>
2023-07-09 00:06:09 +0300

    Revert "blog-impl (#5)" (#6)
    This reverts commit 623f5a10c7cdd5495d432ef51fb71305e73289f5.

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at']
-    list_filter = ['created_at']
-    search_fields = ['title']
-
-admin.site.register(Post, PostAdmin)
--- jarhost_back/urls.py
+++ jarhost_back/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/jars/', include('jars.urls')),
     path('auth/', include('authentication.urls')),
-    path('blog/', include('blog.urls'))
 ]
