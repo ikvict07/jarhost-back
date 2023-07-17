79e7c370824ff4345fc8521f3ad330e8bd03fe85
Netik Viacheslav <slava5582@gmail.com>
2023-07-17 20:35:18 +0200

    Change slug func Refactoring class Image and Video

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,13 @@
 from urllib.parse import urlparse
 
 from django.conf import settings
-# from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.core.validators import URLValidator
+from django.core.validators import URLValidator, FileExtensionValidator
+
 from django.db import models
 from django.utils.text import slugify
 
 
-# Add parameters
 class Squad(models.Model):
     name = models.CharField(max_length=70, unique=True, verbose_name='Назва підрозділу')
     description = models.TextField(help_text="Напішіть про ваш підрозділ", verbose_name="Про підрозділ",
@@ -19,7 +17,6 @@ class Squad(models.Model):
         return self.name
 
 
-# Check class User
 class News(models.Model):
     squad = models.ForeignKey(Squad, on_delete=models.CASCADE, related_name='news')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author')
@@ -32,9 +29,10 @@ class News(models.Model):
     slug = models.SlugField(max_length=10, unique=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        if not self.id:
             super().save(*args, **kwargs)
+        self.slug = slugify(f'{self.title}-{self.id}')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -43,47 +41,46 @@ class News(models.Model):
 class Comment(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField(max_length=200, verbose_name="Ваш коментар")                     # Need to add validator
+    content = models.TextField(max_length=200, verbose_name="Ваш коментар")  # Need to add validator
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # Author does`t have attribute username
     def __str__(self):
-        return f'Comment by {self.author.username}'             # Need to check class User, must be field with username
-        # or change to username to other name
+        return f'Comment by {self.author.username}'  # Need to check class User, must be field with username
+        # or we can to use django.contrib.auth.models.User
 
 
-# Check path to upload
+def get_upload_to(instance, filename):
+    return f'images/{instance.news.author.username}/{filename}'
+
+
+# Add date validation to upload rules
 class Image(models.Model):
-    image = models.ImageField(upload_to='news_images/')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=get_upload_to,
+                              validators=[FileExtensionValidator(['jpg', 'jpeg', 'png'])])
     description = models.TextField(blank=True)
     # uploaded_at = models.DateTimeField(auto_now_add=True)                    # Protect user from load images with date
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
     squad = models.ForeignKey(Squad, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.author = None
-
-    def get_upload_to(self, filename):
-        return 'images/%s/%s' % (self.author.username, filename)
-
     def save(self, *args, **kwargs):
-        if (self.news is None and self.squad is None) or (self.news is not None and self.squad is not None):
-            raise ValueError("Зображення має бути пов'язане або з новиною, або з групою, але не з тим і іншим.")
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Фото для новини {self.news.title}' if self.news else f'Відео для підрозділу {self.squad.name}'
 
 
-def validate_youtube_url(self):
-    parsed = urlparse(self)
-    if "youtube" not in parsed.netloc:
+def validate_youtube_url(value):
+    parsed = urlparse(value)
+    if "youtube" not in parsed.netloc.lower():
         raise ValidationError("Це не дійсна URL-адреса YouTube")
 
 
+# Add date validation to upload rules
 class Video(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     url = models.URLField(validators=[URLValidator(), validate_youtube_url])
     description = models.TextField(blank=True)
     # uploaded_at = models.DateTimeField(auto_now_add=True)                    # Protect user from load images with date
@@ -91,8 +88,6 @@ class Video(models.Model):
     squad = models.ForeignKey(Squad, on_delete=models.CASCADE, related_name='videos', null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if (self.news is None and self.squad is None) or (self.news is not None and self.squad is not None):
-            raise ValueError("Відео має бути пов'язане або з новиною, або з групою, але не з обома.")
         super().save(*args, **kwargs)
 
     def __str__(self):
